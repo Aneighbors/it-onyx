3ad647d3f05c651970cd20671e3d7051f17a6e47
Aneighbors <aneighbors4491@gmail.com>
2023-03-21 22:13:44 +0000

    mars

--- mars-rover/assets/js/mars.js
+++ mars-rover/assets/js/mars.js
@@ -25,58 +25,36 @@ async function GetPhotos() {
      
         var picturedate = document.getElementById("picturedate").value;
         var apiKey = "veO5OolZX0sJ5E77PeGyWQbciSS34TgeMXLA0QOC";
-         
  
         /* URL for AJAX Call */
         var myURL2 = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?earth_date=" + picturedate + "&api_key=" + apiKey;
         /* Make the AJAX call */
-        var msg2Object = await fetch(myURL2);
-        /* Check the status */
-        if (msg2Object.status >= 200 && msg2Object.status <= 299) {            
-            var msg2JSONText = await msg2Object.text();
-            // Parse the JSON string into an object
-            var msg2 = JSON.parse(msg2JSONText);
-            /* Your code to process the result goes here - 
-               display the returned message */
-                /* Your code to process the result goes here  
-                    display the returned message */
-                var closeprice = [];
-                var stockdate= [];
-                var numdays = msg2.results.length;
-                if (numdays > 0) {
-                    for (var i = 0; i < numdays; i++) {
-                        /* stock close value */
-                        closeprice[i] = msg2.results[i].c;
-                        /* date is in Unix milleseconds - create a temporary date variable */
-                        var tempdate = new Date(msg2.results[i].t);
-                        /* extract the date string from the value */
-                        stockdate[i] = tempdate.toLocaleDateString();
-                    }
+
+
+let PictureObject = await fetch(myURL2);
+let Result = await PictureObject.text();
+let msg =JSON.parse(Result); 
+document.getElementById("numberofphotos").
+innerHTML = msg.photos.length;
+if (numberofphotos >25)
+{numberofphotos = 25}
+                
+                for (let i = 0; i < 25; i++) {
+                    // Note how we construct the name for image1, image2, etc...this sets the image source
+                    document.getElementById("image" + i).src = msg.photos[i].img_src;
+                   /* do something to set the tool tip = msg.photos[i].camera.full_name; */
+                   document.getElementById("image" + i).style.visibility = "visible";
                 }
-        }
+            }
         else {
             /* AJAX completed with error - probably invalid stock ticker symbol */
             alert("Stock Not Found - Status: " + msg2Object.status)
             return
         }
     }
-}
-
-function ClearForm() {
+function clearorm() {
     document.getElementById("FromDate").value = "";
     document.getElementById("ToDate").value = "";
     document.getElementById("selconvertcurrency").checked = false;
 
-
-  
-    
-    
-    
-    /* Ugly Code to Erase Canvas */
-    var canvas0 = document.getElementById("chartjs-0");
-    var context0 = canvas0.getContext('2d');    
-    context0.clearRect(0, 0, canvas0.width, canvas0.height);
-    var canvas1 = document.getElementById("chartjs-1");
-    var context1 = canvas1.getContext('2d');    
-    context1.clearRect(0, 0, canvas1.width, canvas1.height);
 }
