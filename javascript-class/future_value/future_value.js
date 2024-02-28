9e2f70decb4c8414e4151c54b02e47b56a76f4a8
Aneighbors <aneighbors4491@gmail.com>
2024-02-28 18:54:07 +0000

    ch4ex

---
+++ javascript-class/future_value/future_value.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+const calculateFV = (investment, rate, years) => {
+    let futureValue = investment;
+    for (let i = 1; i <= years; i++ ) {
+        futureValue += futureValue * rate / 100;
+    }
+    futureValue = futureValue.toFixed(2);
+    return futureValue;
+};
+
+ /*const processEntries = () => {
+    const investment = parseFloat($("#investment").value);
+    const rate = parseFloat($("#rate").value);
+    const years = parseInt($("#years").value);
+    let errorMessage = "";
+
+   if (isNaN(investment) || investment <= 0 || investment > 100000) {
+        errorMessage = "Investment is a number that's greater than zero and less than or equal to 100,000";
+         $("#investment").focus();
+    } else if (isNaN(rate) || rate <= 0 || rate > 15) {
+       errorMessage = "Interest rate is a number that's greater than zero and less than or equal to 15";
+        $("#rate").focus();
+   } else if (isNaN(years) || years <= 0 || years > 50) {
+       errorMessage = "Years is a number that's greater than zero and less than or equal to 50";
+      $("#years").focus();
+   }
+ if (errorMessage == "") {
+         $("#future_value").value = calculateFV(investment, rate, years);        
+     } else {
+         alert(errorMessage);
+   }
+ }
+ */
+ document.addEventListener("DOMContentLoaded", () => {
+    $("#calculate").addEventListener("click", () => {
+        const investment = parseFloat($("#investment").value);
+        const rate = parseFloat($("#rate").value);
+        const years = parseInt($("#years").value);
+        let errorMessage = "";
+    
+        if (isNaN(investment) || investment <= 0 || investment > 100000) {
+            errorMessage = "Investment is a number that's greater than zero and less than or equal to 100,000";
+            $("#investment").focus();
+        } else if (isNaN(rate) || rate <= 0 || rate > 15) {
+            errorMessage = "Interest rate is a number that's greater than zero and less than or equal to 15";
+            $("#rate").focus();
+        } else if (isNaN(years) || years <= 0 || years > 50) {
+            errorMessage = "Years is a number that's greater than zero and less than or equal to 50";
+            $("#years").focus();
+        }
+        if (errorMessage == "") {
+            $("#future_value").value = calculateFV(investment, rate, years);        
+        } else {
+            alert(errorMessage);
+        }
+    });
+    $("#investment").focus();
+});
