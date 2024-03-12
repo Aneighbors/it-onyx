7a15613503bcdaa47547c04876582e82efbcbc38
Aneighbors <aneighbors4491@gmail.com>
2024-03-12 20:35:14 +0000

    update

---
+++ javascript-class/book_list/book_list.js
@@ -0,0 +1,34 @@
+"use strict";
+
+$(document).ready( () =>{
+ 
+    $("#categories a").each( (index, link) => {
+      const image = new Image();
+      image.src= link.href;
+    });
+
+    $("#categories a").click (evt => {
+        const link = evt.currentTarget;
+
+        $("#image").attr("src", link.href);
+
+        evt.preventDefault();
+    })
+
+    $("#categories h2").click( evt => {
+        const h2 = evt.currentTarget;
+        $(h2).toggleClass("minus");
+
+        if ($(h2).attr("class") !== "minus") {
+            $(h2).next().hide();
+        }
+        else{
+            $(h2).next().show();
+        }
+
+        evt.preventDefault();
+    });
+
+
+
+});
